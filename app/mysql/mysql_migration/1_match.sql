0e366b0b041eb4bf2a5a015feed7e9f212959594
Ynakamot <nakamo326@gmail.com>
2024-02-13 00:41:59 +0900

    userのクエリまとめ

--- app/mysql/mysql_migration/1_match.sql
+++ app/mysql/mysql_migration/1_match.sql
@@ -1,2 +1,3 @@
 ALTER TABLE match_group_member ADD INDEX match_user_index (user_id);
 ALTER TABLE match_group_member ADD INDEX match_group_id_index (match_group_id);
+ALTER TABLE match_group ADD INDEX match_group_name_index (match_group_name);
