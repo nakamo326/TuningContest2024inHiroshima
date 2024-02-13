6dbed37a6e550c128a1882fde2de0ca6fe564cec
Ynakamot <nakamo326@gmail.com>
2024-02-13 13:37:35 +0900

    add index

--- app/mysql/mysql_migration/1_match.sql
+++ app/mysql/mysql_migration/1_match.sql
@@ -1,2 +1,3 @@
 ALTER TABLE match_group_member ADD INDEX match_user_index (user_id);
 ALTER TABLE match_group_member ADD INDEX match_group_id_index (match_group_id);
+ALTER TABLE match_group ADD INDEX mg_id_s_idx (match_group_id, status);
