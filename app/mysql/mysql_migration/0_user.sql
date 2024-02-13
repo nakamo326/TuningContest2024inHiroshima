dd2f7cd5d9d712b9cfd92d78f37b67c5fce39c66
Ynakamot <nakamo326@gmail.com>
2024-02-13 12:28:18 +0900

    Revert "Revert "Revert "userのクエリまとめ"""
    This reverts commit 72302d56169400aaec49496776bef6e2adae22d9.

--- app/mysql/mysql_migration/0_user.sql
+++ app/mysql/mysql_migration/0_user.sql
@@ -4,14 +4,6 @@ ALTER TABLE user ADD INDEX usericon_index (user_icon_id);
 ALTER TABLE user ADD INDEX office_id_index (office_id);
 ALTER TABLE user ADD INDEX emp_index (employee_id);
 ALTER TABLE user ADD INDEX mailpass_index (mail, password);
-
-ALTER TABLE `user` DROP PRIMARY KEY,
-ADD COLUMN `id` INT NOT NULL AUTO_INCREMENT FIRST,
-ADD PRIMARY KEY (`id`),
-ADD UNIQUE (`user_id`);
-ALTER TABLE `user` ADD INDEX `user_id_index` (`user_id`);
-
-
 CREATE FULLTEXT INDEX idx_ngram_user_name ON user (user_name) WITH PARSER ngram;
 CREATE FULLTEXT INDEX idx_ngram_kana ON user (kana) WITH PARSER ngram;
 CREATE FULLTEXT INDEX idx_ngram_mail ON user (mail) WITH PARSER ngram;
